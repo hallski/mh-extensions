c23adf7be0c70523ff2a5e11c0430b84d1053c2e
Mikael Hallendal <mhallendal@gmail.com>
2011-02-18 13:54:35 +0100

    Initial Commit

---
+++ MHExtensionsTests/MHExtensionsTests.h
@@ -0,0 +1,17 @@
+//
+//  MHExtensionsTests.h
+//  MHExtensionsTests
+//
+//  Created by Mikael Hallendal on 2011-02-18.
+//  Copyright 2011 Mikael Hallendal. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface MHExtensionsTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ MHExtensionsTests/MHExtensionsTests.m
@@ -0,0 +1,33 @@
+//
+//  MHExtensionsTests.m
+//  MHExtensionsTests
+//
+//  Created by Mikael Hallendal on 2011-02-18.
+//  Copyright 2011 Mikael Hallendal. All rights reserved.
+//
+
+#import "MHExtensionsTests.h"
+
+
+@implementation MHExtensionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MHExtensionsTests");
+}
+
+@end
